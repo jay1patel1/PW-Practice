4068aa580a758ec6c74bd5600bca96dde4294368
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-02-08 17:13:17 +0000

    updated

---
+++ 1.js
@@ -0,0 +1,15 @@
+console.log(null === undefined);
+console.log(null === null);
+console.log(undefined === undefined);
+console.log(NaN == NaN);
+
+console.log (isNaN(NaN));
+
+console.log(1110.34567.toPrecision (5));
+console.log(1110.34567.toFixed (2));
+console.log(1110.64567.toFixed (0));
+
+let b = true.toString()
+console.log(typeof b);
+let c = NaN
+console.log(c.toString(), typeof c);
