6fa8904c0c617947aa79ccb8e33007e62fb7e47a
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-02-10 17:32:45 +0530

    updated

--- 11.js
+++ 11.js
@@ -1,1 +1,12 @@
-me
+let x = [];
+console.log(x);
+let arr = [10,"jay",true,false,undefined,false,33.33,53]
+console.log(arr)
+console.log(arr.length);
+let array = [4,5,6,6]
+array.push(10);
+console.log(array)
+array.unshift(-1);
+console.log(array)
+array.shift()
+console.log(array)
